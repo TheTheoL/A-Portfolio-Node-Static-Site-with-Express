21f2ab11c8cec769daba01a3cfbc72fb59bb5988
Theo L <atlufkin@outlook.com>
2022-01-04 14:02:50 -0500

    Update
    changed project route to /:id and corrected the names in my view.

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { projects } = require('../data/data.json');
 
 
-router.get("/projects/:id", (req, res, next) =>{
+router.get("/:id", (req, res, next) =>{
    return res.render('projects', projects[req.params.id]);
 });
 
